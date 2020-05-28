42462f4997124308aeb04ac47029c1c1b920a430
Manuel Gandul <manu.gandul@gmail.com>
2020-05-28 18:05:36 +0200

    Changed MongoDB for MariaDB

--- covid/src/main/java/com/upo/eps/in/covid/repository/DataRepository.java
+++ covid/src/main/java/com/upo/eps/in/covid/repository/DataRepository.java
@@ -1,9 +1,11 @@
 package com.upo.eps.in.covid.repository;
 
 import com.upo.eps.in.covid.entity.Data;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+
 @Repository("DataRepository")
-public interface DataRepository extends MongoRepository<Data, String> {
+public interface DataRepository extends JpaRepository<Data, Serializable> {
 }
